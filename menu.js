bd2c6b74aed6b36acf03070e541114e95dfd76e7
Santiago Pochettino <santiagopochettino91@gmail.com>
2022-03-01 20:15:34 -0300

    [ADD] First commit

---
+++ menu.js
@@ -0,0 +1,13 @@
+function irArriba(pxPantalla){
+    window.addEventListener('scroll',()=>{
+        var scroll = document.documentElement.scrollTop;
+        var botonArriba = document.getElementById('botonArriba');
+
+        if(scroll>pxPantalla){
+            botonArriba.style.right = 20 + "px";
+        } else{
+            botonArriba.style.right = -100 + "px";
+        }
+    })
+}
+irArriba(500);
